1bf3ddb4d288d1168f8b6cb3ff0692c2cd165bc9
ismaelGonzales <smile.storee@gmial.com>
2025-04-22 11:12:27 -0500

    Guardo cambios locales en contentTypes.d.ts

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -644,7 +644,7 @@ export interface ApiReservaReserva extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
-    codigoReserva: Schema.Attribute.String;
+    codigoReserva: Schema.Attribute.UID;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
